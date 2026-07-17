60ca97adf772de423d6d69d7a9849a7f04b6efc2
dev-karani <edwin.k.kithinji@gmail.com>
2026-07-17 12:51:24 +0300

    feat: implement auth get token func

---
+++ internal/auth/headers.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+func GetBearerToken(headers http.Header) (string, error) {
+	// confirm authorization header
+	authstring := headers.Get("authorization")
+	if authstring == "" {
+		return "", errors.New("missing authorization header")
+	}
+
+	// confirm beraer token prefix
+	var bearerPrefix = "Bearer "
+
+	if !strings.HasPrefix(authstring, bearerPrefix) {
+		return "", errors.New("authorization header must start with Bearer")
+	}
+
+	// get bearer Token
+	token := strings.TrimPrefix(authstring, bearerPrefix)
+	if token == "" {
+		return "", errors.New("missing bearer token")
+	}
+
+	return token, nil
+}
