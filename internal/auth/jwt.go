353cf3f41b4192a5a2043b407ff96db4c981b7fd
dev-karani <edwin.k.kithinji@gmail.com>
2026-07-18 14:23:21 +0300

    feat: add makejwt func & validate jwt func

---
+++ internal/auth/jwt.go
@@ -0,0 +1,49 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func MakeJwt(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	//define claims structure
+	//RETURN tokenSecret
+
+	claims := jwt.RegisteredClaims{
+		Issuer:    "fintrack-access",
+		IssuedAt:  jwt.NumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := &jwt.RegisteredClaims{}
+
+	_, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	userIDStr := claims.Subject
+
+	userID, err := uuid.Parse(userIDStr)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return userID, nil
+}
