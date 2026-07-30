b978fd01e36e3e8c9a396060160cfb5ecdc38813
dev-karani <edwin.k.kithinji@gmail.com>
2026-07-30 12:59:50 +0300

    chore: refactor transaction queries

--- sql/queries/transactions.sql
+++ sql/queries/transactions.sql
@@ -16,13 +16,13 @@ RETURNING *;
 -- name: GetAllTransactionsByUserID :many
 SELECT * FROM transactions
 WHERE user_id= $1
+AND deleted_at IS NULL
 ORDER BY created_at ASC;
 
 -- name: GetTransactionByID :one
 SELECT * FROM transactions
 WHERE id=$1
-AND deleted_at=NULL
-RETURNING *;
+AND deleted_at IS NULL;
 
 -- name: UpdateTransactionByID :one
 UPDATE transactions
@@ -34,11 +34,13 @@ SET
     destination=$7
 WHERE id=$1
 AND user_id=$2
-AND deleted_at=NULL
+AND deleted_at IS NULL
 RETURNING *;
 
 -- name: DeleteTransactionBYID :exec
 UPDATE transactions
 SET
     deleted_at=True
-WHERE id=$1;
+WHERE id=$1
+AND deleted_at IS NULL
+RETURNING *;
