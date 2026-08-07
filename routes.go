b068550f6642b426a35e69fe4990d52df224caa9
dev-karani <edwin.k.kithinji@gmail.com>
2026-08-07 17:03:01 +0300

    feat:implement balance, expenses, income routes

--- routes.go
+++ routes.go
@@ -39,7 +39,10 @@ func registerRoutes(
 
 	mux.HandleFunc("POST "+apiV1+"/transactions", transactionsHandler.CreateTransaction)
 	mux.HandleFunc("GET "+apiV1+"/transactions", transactionsHandler.GetAllUserTransactions)
-	//mux.HandleFunc("GET " +apiV1+"/balance", transactionsHandler.)
+
+	mux.HandleFunc("GET "+apiV1+"/balance", transactionsHandler.GetUserBalance)
+	mux.HandleFunc("GET "+apiV1+"/income", transactionsHandler.GetUserIncome)
+	mux.HandleFunc("GET "+apiV1+"/expenses", transactionsHandler.GetUserExpenses)
 
 	mux.HandleFunc("GET "+apiV1+"/transactions/{transactionID}", transactionsHandler.GetTransactionByID)
 	mux.HandleFunc("PUT "+apiV1+"/transactions/{transactionID}", transactionsHandler.UpdateTransaction)
